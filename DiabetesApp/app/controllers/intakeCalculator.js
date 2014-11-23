523d7c81482070a387bc9d1104707f76a9320619
j <treefort@js-MacBook-Pro.local>
2014-11-23 15:48:55 -0800

    Added TDD calculation. Added transition to nutrition states view, defined in JSON.

--- DiabetesApp/app/controllers/intakeCalculator.js
+++ DiabetesApp/app/controllers/intakeCalculator.js
@@ -3,40 +3,23 @@ var intakeCalculatorApp = angular.module('intakeCalculatorApp', ['IntakeCalculat
 
 // Index: http://localhost/views/intakeCalculator/index.html
 
-intakeCalculatorApp.controller('IndexCtrl', function ($scope, IntakeCalculatorRestangular) {
+// intakeCalculatorApp.controller('IndexCtrl', function ($scope, IntakeCalculatorRestangular) {
 
-  // Helper function for opening new webviews
-  $scope.open = function(id) {
-    webView = new steroids.views.WebView("/views/intakeCalculator/show.html?id="+id);
-    steroids.layers.push(webView);
-  };
+//   // Helper function for opening new webviews
+//   $scope.open = function(id) {
+//     webView = new steroids.views.WebView("/views/intakeCalculator/show.html?id="+id);
+//     steroids.layers.push(webView);
+//   };
 
-  // Fetch all objects from the local JSON (see app/models/intakeCalculator.js)
-  IntakeCalculatorRestangular.all('intakeCalculator').getList().then( function(intakeCalculators) {
-    $scope.intakeCalculators = intakeCalculators;
-  });
+//   // Fetch all objects from the local JSON (see app/models/intakeCalculator.js)
+//   // IntakeCalculatorRestangular.all('intakeCalculator').getList().then( function(intakeCalculators) {
+//   //   $scope.intakeCalculators = intakeCalculators;
+//   // });
 
-  // Native navigation
-  steroids.view.navigationBar.show("Intake Calculator");
-  steroids.view.setBackgroundColor("#FFFFFF");
-});
-
-
-// Show: http://localhost/views/intakeCalculator/show.html?id=<id>
-
-intakeCalculatorApp.controller('ShowCtrl', function ($scope, $filter, IntakeCalculatorRestangular) {
-
-  // Fetch all objects from the local JSON (see app/models/intakeCalculator.js)
-  IntakeCalculatorRestangular.all('intakeCalculator').getList().then( function(intakeCalculators) {
-    // Then select the one based on the view's id query parameter
-    $scope.intakeCalculator = $filter('filter')(intakeCalculators, {id: steroids.view.params['id']})[0];
-  });
-
-  // Native navigation
-  steroids.view.navigationBar.show("IntakeCalculator: " + steroids.view.params.id );
-  steroids.view.setBackgroundColor("#FFFFFF");
-
-});
+//   // Native navigation
+//   steroids.view.navigationBar.show("Intake Calculator");
+//   steroids.view.setBackgroundColor("#FFFFFF");
+// });
 
 // Bind patient stats
 intakeCalculatorApp.controller('StatsCtrl', function($scope) {
@@ -45,36 +28,75 @@ intakeCalculatorApp.controller('StatsCtrl', function($scope) {
     bmi:''
   };
 
-  $scope.pancreatectomy = false;
-  $scope.steroids = false;
-  $scope.over70 = false;
-  $scope.bmiClass = 'Test'
+  $scope.bmiClass = '';
+  $scope.tdd = '';
 
+
+  // Internal functions
   function between(x, min, max) {
     return x >= min && x <= max;
   }
 
-   var computeBmiClass = function() {
-    if ($scope.stats.bmi <= 18.5) {
-      $scope.bmiClass = "Malnourished";
-    } else if (between($scope.stats.bmi, 18.51, 24.999)) {
-      $scope.bmiClass = "Lean";
-    } else if (between($scope.stats.bmi, 25, 30)) {
-      $scope.bmiClass = "Overweight";
-    } else {
-      $scope.bmiClass = "Obese";
+  var calculateTdd = function() {
+    try {
+      if ($scope.stats.weight !== '' && $scope.bmiClass !== '') {
+        $scope.tdd = Math.round($scope.bmiClass[1] * $scope.stats.weight * 1000)/1000;
+      }
+    } catch (e) {
+      // NaN error, sanitize inputs
     }
   }
 
-  $scope.$watch('stats.bmi', computeBmiClass);
+  var computeBmiClass = function() {
+    if ($scope.stats.bmi <= 18.5) {
+      $scope.bmiClass = ["Malnourished", 0.3];
+    } else if (between($scope.stats.bmi, 18.51, 24.999)) {
+      $scope.bmiClass = ["Lean", 0.4];
+    } else if (between($scope.stats.bmi, 25, 30)) {
+      $scope.bmiClass = ["Overweight", 0.5];
+    } else {
+      $scope.bmiClass = ["Obese", 0.6];
+    }
 
+    calculateTdd();
+  }
+
+  // External Functions
+  $scope.validateInputs = function() {
+    if ($scope.stats.bmi !== '' && $scope.stats.weight !== '') {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  $scope.open = function(tdd) {
+    webView = new steroids.views.WebView("/views/intakeCalculator/nutritionalState.html?tdd="+$scope.tdd);
+    steroids.layers.push(webView);
+  };
+
+  // Observers
+  $scope.$watch('stats.bmi', computeBmiClass);
+  $scope.$watch('stats.weight', calculateTdd)
 });
 
-intakeCalculatorApp.controller('PrescreenCtrl', function($scope) {
-  $scope.screening = {
-    dm1: 0,
-    dm2: 0,
-    pancreatectomy: 0,
-    steroids: 0
+// Show: http://localhost/views/intakeCalculator/show.html?id=<id>
+
+intakeCalculatorApp.controller('NutrtitionalStateCtrl', function ($scope, $filter, IntakeCalculatorRestangular) {
+
+  // Fetch all objects from the local JSON (see app/models/intakeCalculator.js)
+  IntakeCalculatorRestangular.all('nutritionStates').getList().then( function(nutritionalStates) {
+    // Then select the one based on the view's id query parameter
+    $scope.nutritionalStates = nutritionalStates;
+  });
+
+  $scope.open = function() {
+    webView = new steroids.views.WebView("/views/intakeCalculator/nutritionalState.html");
+    steroids.layers.push(webView);
   }
+
+  // Native navigation
+  steroids.view.navigationBar.show("Nutrition States");
+  steroids.view.setBackgroundColor("#FFFFFF");
+
 });
